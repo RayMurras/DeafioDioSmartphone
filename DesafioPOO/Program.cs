ff9e3ccd1a28fe7f6bafc5ca07ddca059e31d00d
Rayanne Murras <72751669+RayMurras@users.noreply.github.com>
2023-07-25 21:46:57 +0100

    Add files via upload

---
+++ DesafioPOO/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+System.Console.WriteLine("Smatphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "22222222", modelo: "Nokia 2", imei: "55555", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Candy Crush");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smatphone Iphone: ");
+Smartphone iphone = new Nokia(numero: "22222222", modelo: "Iphone 12", imei: "55555", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Candy Crush");
